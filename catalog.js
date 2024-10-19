4ef9e85481ca6544400d0ce79d5754b392aa3c5c
jeffhhhh <jeffreyshawnremy@gmail.com>
2024-10-19 18:09:09 -0500

    Added ability to filter by genre on catalog page. Genre options are automatically generated by countGenres() in catalog.js

--- catalog.js
+++ catalog.js
@@ -1,32 +1,53 @@
-function loadAlbums(albums, filter) {
+function loadAlbums(albums, genre, search) {
     const arrayAlbums = albums.albums;
 
-    console.log(arrayAlbums);
+    // get all genres and number of albums with each given genre 
+    let genreCounts = countGenres(arrayAlbums);
 
-    // by default, show all albums without a filter
-    let filteredAlbums = arrayAlbums;
+    // use genreCounts to construct genre options for genre filter dropdown
+    let genreDropdown = document.getElementById("genre-selector");
+    genreDropdown.innerHTML = `
+        <li><a class="dropdown-item" href="#" onclick="displayAlbumsDefault()">All Genres</a></li>
+        <li><hr class="dropdown-divider"></li>
+    `;
+    for (let genrePair of genreCounts) {
+        let genreOption = document.createElement("li");
 
-    // if filter is nonempty, check for a more specific filter to apply to filteredAlbums
-    if (filter) {
+        // create each genre option with name and number of albums in the catalog associated with the genre
+        genreOption.innerHTML = `
+            <li><a class="dropdown-item" href="#" onclick="displayAlbumsByGenre('${genrePair[0]}')">${genrePair[0]} (${genrePair[1]})</a></li>
+        `;
+        genreDropdown.appendChild(genreOption);
+    }
 
+    
+    let filteredAlbums = [];
+    // if genre is nonempty, filter by that genre
+    console.log(genre);
+    if (genre) {
+        for (let album of arrayAlbums) {
+            // only include album if it has the specified genre
+            if (album.genres.includes(genre)) {
+                filteredAlbums.push(album);
+            }
+        }
+    }
+    // otherwise, show all albums 
+    else {
+        filteredAlbums = arrayAlbums;
     }
 
     var albumCatalog = document.getElementById("album-catalog");
     albumCatalog.innerHTML = "";
 
-    console.log(filteredAlbums.length);
     for (let i = 0; i < filteredAlbums.length; i++) {
         let album = filteredAlbums[i];
-
-        console.log(album);
         
         let title = album.name;
         let artist = album.artist;
         let year = album.year;
         let genres = album.genres;
         let cover = album.cover;
-
-        console.log(title);
 
         // construct Bootstrap card containing album details
         let albumCard = document.createElement("div");
@@ -46,11 +67,49 @@ function loadAlbums(albums, filter) {
     }
 }
 
-function defaultDisplayAlbums() {
+// go through the given albums and count the genres they are described as
+// return a sorted map with genre name keys and integer values representing the count (i.e., "Rock": 5)
+function countGenres(albums) {
+    let genreCounts = new Map();
+    for (let i = 0; i < albums.length; i++) {
+        let album = albums[i];
+
+        // iterate through the album's genres
+        for (let j = 0; j < album.genres.length; j++) {
+            let genre = album.genres[j];
+            // check if a given genre has already been found
+            if (genreCounts.has(genre)) {
+                // if so, increment the value by 1
+                let currCount = genreCounts.get(genre);
+                genreCounts.set(genre, currCount + 1);
+            }
+            // if it has not been found, add it to the map
+            else {
+                genreCounts.set(genre, 1);
+            }
+        }
+    }
+
+    // create a sorted map in decreasing order of counts
+    let sortedMap = new Map([...genreCounts.entries()].sort((a, b) => b[1] - a[1]))
+
+    // return final sorted map
+    return sortedMap;
+}
+
+function displayAlbumsDefault() {
     fetch("./data.json")
         .then(response => response.json())
-        .then(albums => loadAlbums(albums, ""))
+        .then(albums => loadAlbums(albums, "", ""))
         .catch(err => console.log("Error: " + err)); 
 }
 
-defaultDisplayAlbums();
+// load all albums matching a specified genre
+function displayAlbumsByGenre(genre) {
+    fetch("./data.json")
+        .then(response => response.json())
+        .then(albums => loadAlbums(albums, genre, ""))
+        .catch(err => console.log("Error: " + err)); 
+}
+
+displayAlbumsDefault();
